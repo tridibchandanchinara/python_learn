cb0d05a21c1f1fe2835dc99fba7ebce800b1c105
Chandan <tchinara@gemini-us.com>
2024-06-27 09:00:39 +0530

    first commit

---
+++ webapp/views.py
@@ -0,0 +1,18 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+def home_view(request):
+    return render(request, 'webapp/home.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or dashboard after login
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'webapp/login.html')
+
